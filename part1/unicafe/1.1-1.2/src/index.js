c8fa6ebf89f7ce5a501eddc03d9dc35a345dad89
unknown <jul.1012@hotmail.com>
2023-07-10 19:50:27 -0500

    finished unicafe

--- part1/unicafe/1.1-1.2/src/index.js
+++ part1/unicafe/1.1-1.2/src/index.js
@@ -1,69 +1,125 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { useState,useEffect } from 'react'
 
 
-const Part = (props) => {
+
+const StatisticsLine = ({text}) =>{
   return (
-    <p>{props.part} {props.exercises}</p>
-  );
-  
-
+    <p>{text}</p>
+  )
 
 }
 
-const Header = (props) =>{
-  return (
-    <h1>{props.course}</h1>
-  );
 
-}
+const Statistics = ({good,neutral,bad,allFeedbacks,average,postiveFeedbacks}) =>{
 
-const Content = (props) =>{
-  const parts = props.parts
-
-  const component = parts.map((value) =>{
+  if(allFeedbacks === 0){
     return (
-      <Part  part={value.name} exercises={value.exercises}></Part>
+      <h1>No feedback given</h1>
     )
 
-  })
-
-
-  return component
-}
-
-const Total = (props) =>{
-  return (
-    <p>Number of exercises {props.number}</p>
-  );
-}
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
   }
-  
+
 
   return (
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}></Content>
-      <Total number={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}></Total>
-    </div>
+    <table>
+      <tr>
+        <td><p>good</p></td>
+        <td><StatisticsLine text={good}></StatisticsLine></td>
+      </tr>
+      <tr>
+        <td><p>neutral</p></td>
+        <td><StatisticsLine text={neutral}></StatisticsLine></td>
+      </tr>
+      <tr>
+        <td><p>bad</p></td>
+        <td><StatisticsLine text={bad}></StatisticsLine></td>
+      </tr>
+      <tr>
+        <td><p>all</p></td>
+        <td><StatisticsLine text={allFeedbacks}></StatisticsLine></td>
+      </tr>
+      <tr>
+        <td><p>average</p></td>
+        <td><StatisticsLine text={average}></StatisticsLine></td>
+      </tr>
+      <tr>
+        <td><p>positive</p></td>
+        <td><StatisticsLine text={postiveFeedbacks + "%"}></StatisticsLine></td>
+      </tr>
+    </table>
+  )
+
+
+}
+
+const Button = ({click,text}) =>{
+  return(
+    <button onClick={click}>{text}</button>
   )
 }
 
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all,setAll] = useState(0)
+  const [avgset,setAvg] =useState(0)
+  const [positive,setPositive] =useState(0)
+
+
+  const setFeedGood = () => {
+    setGood(good + 1);
+    setAll(all + 1)
+  };
+  
+  const setFeedNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1)
+  };
+  
+  const setFeedBad = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  };
+
+  
+  
+  
+  useEffect(() => {
+    const calculateAverage = () => {
+      let sum = good - bad;
+      setAvg(sum/all || 0);
+    };
+
+    const calculatePositiveFeedback = () => {
+      let percentage = (good * 100) / all;
+      setPositive(percentage || 0);
+    };
+
+    calculateAverage();
+    calculatePositiveFeedback();
+  }, [good, bad, all]);
+  
+
+
+ 
+
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button click={() => setFeedGood()} text='good'></Button>
+      <Button click={() => setFeedNeutral()} text='neutral'></Button>
+      <Button click={() => setFeedBad()} text='bad'></Button>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} allFeedbacks={all} average={avgset} postiveFeedbacks={positive} ></Statistics>
+      
+    </div>
+  )
+}
 ReactDOM.render(<App />, document.getElementById('root'))
